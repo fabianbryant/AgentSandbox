12857d34fa57dc17cc8315574b931e6712a306e1
fabianbryant <bryant.vichon@gmail.com>
2026-09-20 13:32:14 -0500

    Consolidate paths into config variables in run script

--- run.sh
+++ run.sh
@@ -6,19 +6,26 @@ set -eux
 : ${SHM_SIZE:=1g}
 : ${TMPFS_SIZE:=256m}
 
-mkdir -p "$HOME/.${TARGET}" \
-    "$PWD/agents/${TARGET}/.local" \
-    "$PWD/agents/${TARGET}/share/ro" \
-    "$PWD/agents/${TARGET}/share/rw"
+: ${HOST_AGENTS_DIR:=$PWD/agents}
+: ${HOST_AGENT_HOME_DIR:=$HOME/.${TARGET}}
+
+HOST_AGENT_DOT_LOCAL_DIR="${HOST_AGENTS_DIR}/${TARGET}/.local"
+HOST_AGENT_SHARE_RO_DIR="${HOST_AGENTS_DIR}/${TARGET}/share/ro"
+HOST_AGENT_SHARE_RW_DIR="${HOST_AGENTS_DIR}/${TARGET}/share/rw"
+
+mkdir -p "${HOST_AGENT_HOME_DIR}" \
+    "${HOST_AGENT_DOT_LOCAL_DIR}" \
+    "${HOST_AGENT_SHARE_RO_DIR}" \
+    "${HOST_AGENT_SHARE_RW_DIR}"
 
 docker run --rm -it \
     --cap-drop ALL \
     --security-opt no-new-privileges:true \
     --shm-size $SHM_SIZE \
     --tmpfs "/tmp:rw,noexec,nosuid,size=$TMPFS_SIZE" \
-    -v "$HOME/.${TARGET}:/home/agent/.${TARGET}" \
-    -v "$PWD/agents/${TARGET}/.local:/home/agent/.local" \
-    -v "$PWD/agents/${TARGET}/share/rw:/home/agent/share/rw" \
-    -v "$PWD/agents/${TARGET}/share/ro:/home/agent/share/ro:ro" \
+    -v "${HOST_AGENT_HOME_DIR}:/home/agent/.${TARGET}" \
+    -v "${HOST_AGENT_DOT_LOCAL_DIR}:/home/agent/.local" \
+    -v "${HOST_AGENT_SHARE_RW_DIR}:/home/agent/share/rw" \
+    -v "${HOST_AGENT_SHARE_RO_DIR}:/home/agent/share/ro:ro" \
     "$IMAGE_NAME" \
     "$@"
