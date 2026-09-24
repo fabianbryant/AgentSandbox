a259eb50af595fb1ca46ab977b470c707c374b02
fabianbryant <bryant.vichon@gmail.com>
2026-09-23 22:56:24 -0500

    Make entrypoint address dynamic home: $HOME/.local/bin

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-mkdir -p /home/agent/.local/bin
+mkdir -p "$HOME/.local/bin"
 
 if [[ ! $PATH =~ '.local/bin' ]]; then
-    export PATH="/home/agent/.local/bin:$PATH"
+    export PATH="$HOME/.local/bin:$PATH"
 fi
 
 exec "$@"
